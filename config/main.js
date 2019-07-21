15648167e8960b2481d911b5ff0ab822a0a688ea
duycs <duycs@outlook.com>
2019-07-21 20:29:56 +0700

    change port to 5000

--- config/main.js
+++ config/main.js
@@ -16,7 +16,7 @@ module.exports = {
   // Stripe API key
   stripeApiKey: 'stripe api key goes here',
   // necessary in order to run tests in parallel of the main app
-  test_port: 3001,
+  test_port: 5000,
   test_db: 'chatting-test',
   test_env: 'test'
 };
