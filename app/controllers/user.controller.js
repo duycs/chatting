0c6d5bee07462e90c342a1f38019c3c6e28f7265
Chu Sỹ Duy <duychusy92@gmail.com>
2023-01-27 10:45:35 +0700

    update data content

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -7,17 +7,58 @@ exports.userBoard = (req, res) => {
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+  res.status(200).send(JSON.stringify(managementData));
 };
 
 exports.operatorBoard = (req, res) => {
-  res.status(200).send("Operator Content.");
+  res.status(200).send(JSON.stringify(managementData));
 };
 
 exports.setting = (req, res) => {
-  res.status(200).send("Setting Content.");
+  res.status(200).send(JSON.stringify(settingData));
 };
 
 exports.log = (req, res) => {
-  res.status(200).send("Logs Content.");
+  res.status(200).send(JSON.stringify(logData));
 };
+
+var settingData = {
+  "user" : [
+    {
+      "name" : "user 1",
+      "role" : "operator"
+    },
+    {
+      "name" : "user 2",
+      "role" : "admin"
+    }
+  ],
+  "device" : [
+    {
+      "name" : "device 1",
+      "type" : "type 1"
+    },
+    {
+      "name" : "device 2",
+      "type" : "type 2"
+    }
+  ],
+  "api" : [
+    {
+      "name" : "api 1",
+      "data" : "data 1"
+    }
+  ]
+};
+
+var logData = [
+  "message 1",
+  "message 2",
+  "message 3",
+  "message 4"
+];
+
+var managementData = {
+  "description" : "Test description bla bla bla bla bla bla",
+  "logs" : logData
+}
